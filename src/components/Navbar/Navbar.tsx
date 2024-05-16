0986814e9614f2893c64acee6eb15b7a5d6c158c
Parker Hazel <prkrhzl@gmail.com>
2024-05-16 16:23:00 -0400

    Updated color scheme and styles for Navbar and Credits components. Added socials links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,36 @@
 import styles from './Navbar.module.css';
 
+const destinations = [
+  { name: 'Parker Hazel', href: '/#'},
+  { name: 'Home', href: '/#intro' },
+  { name: 'About', href: '/#about'},
+  { name: 'Experience', href: '/#experience' },
+  { name: 'Projects', href: '/#projects' },
+];
+
+const socials = [
+  { name: 'GitHub', href: 'https://www.github.com/parkerhazel', target: '_blank' },
+  { name: 'LinkedIn', href: 'https://www.linkedin.com/in/parker-hazel', target: '_blank'},
+  { name: 'Email', href: 'mailto:parkerhazell@gmail.com', target: '_blank'},
+]
+
 export const Navbar = () => {
   return (
     <>
       <nav className={styles.Navbar}>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/blog">Blog</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
+        <ul className={styles.NavbarList}>
+          {destinations.map((destination) => (
+            <li key={destination.name} className={styles.NavbarListItem}>
+              <a href={destination.href}>{destination.name}</a>
+            </li>
+          ))}
+        </ul>
+        <ul className={styles.SocialsList}>
+          {socials.map((social) => (
+            <li key={social.name} className={styles.SocialsListItem}>
+              <a href={social.href} target={social.target}>{social.name}</a>
+            </li>
+          ))}
         </ul>
       </nav>
     </>
