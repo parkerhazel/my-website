419f45bbe3423b5e15661756d4cef7d17aa2f329
Parker Hazel <prkrhzl@gmail.com>
2024-05-21 17:46:36 -0400

    Updated TailwindCSS config and Intro component styles

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,16 +1,28 @@
-import styles from './Intro.module.css';
+import styles from "./Intro.module.css";
 
 const Intro = () => {
-    return (
-        <>
-        <div id='intro' className={styles.Intro}>
+  return (
+    <>
+      <div className={styles.Wrapper}>
+        <div id="intro" className={styles.Intro}>
+          <div className={styles.IntroHeader}>
             <h1>Introduction</h1>
+          </div>
+          <div className={styles.paragraph}>
             <p>
-                My name is Parker Hazel, and I&apos;m a Software Engineer.
+              Hello! I&apos;m Parker Hazel, a software engineer with a knack for
+              solving complex problems. I hold a Computer Science degree from
+              the University of North Carolina at Chapel Hill and currently work
+              at Bridge Investment Group. There, I specialize in integrating
+              platforms like AWS and Salesforce and developing web applications
+              with Svelte. My past experience at Gorelick Brothers Capital
+              involved leading Agile projects and optimizing data communication.
             </p>
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Intro;
