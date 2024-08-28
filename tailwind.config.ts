0b6bced7f070665b631c3ba1d5d7ef8010b4b369
Parker Hazel <prkrhzl@gmail.com>
2024-08-28 19:13:35 -0400

    Changed root colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       colors: {
         primary: "#7998cc", // Custom primary color
         secondary: "#bee3b6", // Custom secondary color
